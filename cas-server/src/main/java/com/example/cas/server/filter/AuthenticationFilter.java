b9cee0b7ebcb1f95319fb7b7d561499240f29478
yempty <yempty007@gmail.com>
2020-09-02 00:59:41 +0800

    修改cas-server 鉴权过滤器

--- cas-server/src/main/java/com/example/cas/server/filter/AuthenticationFilter.java
+++ cas-server/src/main/java/com/example/cas/server/filter/AuthenticationFilter.java
@@ -1,18 +1,28 @@
 package com.example.cas.server.filter;
 
 
+import cn.hutool.core.util.StrUtil;
+import com.example.cas.server.common.exception.GlobalException;
+import com.example.cas.server.common.result.ResultCode;
 import org.apache.commons.lang.StringUtils;
+import org.apereo.cas.CipherExecutor;
+import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.ticket.TicketGrantingTicket;
 import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * @Description 登录过滤器
@@ -26,15 +36,22 @@ public class AuthenticationFilter implements Filter {
     private Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
     @Autowired
+    @Qualifier("cookieCipherExecutor")
+    private CipherExecutor cipherExecutor;
+    @Autowired
     private TicketRegistry ticketRegistry;
     /**
      * 校验名单
      */
-    private static final String[] includedUrl = {"/cas/api/**"};
+    private static final String[] INCLUDED_URL = {"/cas/api/**"};
+
+    /**
+     * 管理员账户ID
+     */
+    private static final Long ROOT_ID = 1L;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
@@ -42,17 +59,37 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+
         String uri = request.getRequestURI();
         if (needCheck(uri)) {
             // check authentication
-            String tgc = request.getHeader("TGC");
-            String tgt="";
-            String userName = null;
-            TicketGrantingTicket serviceTicket = (TicketGrantingTicket) ticketRegistry.getTicket(tgt);
-            if (serviceTicket != null && !serviceTicket.isExpired()) {
-                userName = serviceTicket.getAuthentication().getPrincipal().getId();
+            boolean hasPermission = false;
+            String tgc;
+            Cookie cookie = WebUtils.getCookie(request, "TGC");
+            if (cookie != null) {
+                tgc = cookie.getValue();
+            } else {
+                tgc = request.getHeader("TGC");
             }
 
+            String tgt = (String) cipherExecutor.decode(tgc, null);
+            tgt = StrUtil.subBefore(tgt, "@", false);
+
+            if (StrUtil.isNotBlank(tgt)) {
+                TicketGrantingTicket ticket = ticketRegistry.getTicket(tgt, TicketGrantingTicket.class);
+                if (ticket != null && !ticket.isExpired()) {
+                    Principal principal = ticket.getAuthentication().getPrincipal();
+                    Map<String, Object> attributes = principal.getAttributes();
+                    LinkedList idList = (LinkedList) attributes.get("id");
+                    if (ROOT_ID.toString().equals(idList.get(0).toString())) {
+                        hasPermission = true;
+                    }
+                }
+            }
+
+            if (!hasPermission) {
+                throw new GlobalException(ResultCode.FORBIDDEN);
+            }
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
@@ -63,7 +100,7 @@ public class AuthenticationFilter implements Filter {
     }
 
     public static boolean needCheck(String uri) {
-        for (String excluded : includedUrl) {
+        for (String excluded : INCLUDED_URL) {
             if (excluded.contains("/**")) {
                 excluded = excluded.replace("**", "");
                 if (StringUtils.startsWith(uri, excluded)) {
